4ada5975628d25d0d5f3650dc8a682a67870f560
sungaosheng <515798267@qq.com>
2019-10-11 18:54:22 +0800

    add test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
 import NavBar from './navBar'
 import MainWrapper from './mainWrapper'
+import Test from './test'
 import http from 'axios'
 // import { get } from '../../api/axios'
 
 const Home: React.FC = () => {
     // const [data, setData] = useState(0);
+
     useEffect(() => {
         // get('/', {}, res => {
         //     console.log(res);
@@ -22,6 +24,7 @@ const Home: React.FC = () => {
         <div className="home">
             <NavBar />
             <MainWrapper />
+            <Test children={0} />
         </div>
     )
 }
