81a923cfd38047d3889910df6a0de6e475c6169e
A mile <1299285702@qq.com>
2019-10-14 22:47:19 +0800

    add marked and highlight

--- src/pages/home/mainWrapper/mainContent/index.tsx
+++ src/pages/home/mainWrapper/mainContent/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github.css'
 import './index.scss'
 
+const input = '```javascript\nfunction(){\n\tconsole.log(123)\n}\n```';
+const output = marked(input);
+
 const MainContent : React.FC = () => {
+
+    useEffect(() => {
+        marked.setOptions({
+            highlight: code => hljs.highlightAuto(code).value,
+          });
+    });
+
     return (
-        <div></div>
+        <div>
+            <div dangerouslySetInnerHTML={{ __html: output}}></div>            
+        </div>
     )
 }
 
