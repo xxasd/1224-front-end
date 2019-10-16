97bf395dba53f3fa5699f8ee6a01b1197d6e4b5b
sungaosheng <515798267@qq.com>
2019-10-16 18:52:06 +0800

    编写theme.tsx的store

--- src/store/theme.tsx
+++ src/store/theme.tsx
@@ -1,15 +1,19 @@
 import React, { createContext, Context, useReducer } from 'react'
 
-// 初始化store的类型、初始化值、reducer
-export const CHANGE_THEME: string = 'CHANGE_THEME';
-
+// 定义主题色的接口
 interface ITheme {
     theme: string
 }
-
+// 初始化
 export const initialTheme: ITheme = {
     theme: 'light'
 }
+
+// 创建Context状态实例
+export const ThemeContext: Context<any> = createContext(initialTheme);
+
+// 初始化store的类型、初始化值、reducer
+export const CHANGE_THEME: string = 'CHANGE_THEME';
 
 export const reducer = (state: ITheme, action: any) => {
     switch (action.type) {
@@ -20,8 +24,6 @@ export const reducer = (state: ITheme, action: any) => {
     }
 }
 
-// 创建上下文实例
-export const ThemeContext: Context<any> = createContext('light');
 
 // 高阶组件，给函数组件注入上下文
 export const Theme: React.FC = (props) => {
