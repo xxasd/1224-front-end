62e0ef699134192c416e7502bdca70c1fe75a2d7
A mile <1299285702@qq.com>
2020-01-31 10:51:02 +0800

    add icon and author links

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+// 基本配置
+
+// iconfont图标
+export const iconFontUrl = "//at.alicdn.com/t/font_1416353_31e18xp5mng.js";
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,16 @@
 import React, { useContext, useState } from 'react'
-import { Switch } from 'antd'
+import { Switch, Icon } from 'antd'
+import { iconFontUrl } from '@/config'
 import { ThemeContext, CHANGE_THEME } from '@/store/theme'
 import './index.scss'
 
 // 作者头像
 const authorAvatar = "https://wx2.sinaimg.cn/mw690/c990b2b8ly8g5focjdx0jj20u00u0myy.jpg";
+
+// IconFont
+const IconFont = Icon.createFromIconfontCN({
+    scriptUrl: iconFontUrl,
+});
 
 // 首页-欢迎页
 const IndexPages: React.FC = () => {
@@ -25,7 +31,8 @@ const IndexPages: React.FC = () => {
     }
 
     return (
-        <div className={`index-panel ${theme === 'light' ? 'light' : 'dark'}`}>
+        <div className="index-panel">
+            <div className={`index-bg ${theme === 'light' ? 'light' : 'dark'}`}></div>
             <div className="change-theme">
                 <Switch onChange={onChange}></Switch>
             </div>
@@ -40,8 +47,54 @@ const IndexPages: React.FC = () => {
                 <h3>Author: A mile</h3>
                 <p>welcome to give a star in my project</p>
             </div>
+            <div className="index-links">
+                {links.map((item, index) => {
+                    return (
+                        <a 
+                            key={index} 
+                            className="link" 
+                            href={item.link ? item.link : '#'} 
+                            target={item.link ? '_blank': ''}
+                        >
+                            {
+                                item.iconType === 'antd'
+                                    ? <Icon type={item.icon} />
+                                    : <IconFont type={item.icon} />
+                            }
+                        </a>
+                    )
+                })}
+            </div>
         </div>
     )
 }
+
+// 入口链接
+const links = [
+    {
+        name: 'github',
+        icon: 'github',
+        link: 'https://github.com/xxasd/1224-front-end',
+        iconType: 'antd'
+    },
+    {
+        name: 'weibo',
+        icon: 'weibo',
+        link: 'https://weibo.com/u/3381703352/home',
+        iconType: 'antd'
+    },
+    {
+        name: 'wechat',
+        icon: 'wechat',
+        link: null,
+        iconType: 'antd'
+    },
+    {
+        name: 'juejin',
+        icon: 'iconjuejin',
+        link: 'https://juejin.im/user/5d4bbd9fe51d456201486de8',
+        iconType: 'iconfont'
+    }
+]
 
 export default IndexPages
